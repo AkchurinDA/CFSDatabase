a2d5cc7142b32667dd78ccfcdaa887eee13db338
Damir Akchurin <AkchurinDA@gmail.com>
2023-09-05 23:49:15 -0400

    Start processing raw data

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 <div align="center">
 
+
 | Build Status | Latest Release | Downloads | License |
 | :---: | :---: | :---: | :---: |
 | | | | [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) |
@@ -13,9 +14,17 @@
 
 ## References
 
+### Beams
+
 - C. Ding, “Strength and Behavior of Advanced High Strength Steel Structural Components,” PhD Dissertation, Johns Hopkins University, Baltimore, MD, 2022. [Online]. Available: https://jscholarship.library.jhu.edu/handle/1774.2/67848.
 - C. Yu and B. W. Schafer, “Distortional Buckling Tests on Cold-Formed Steel Beams,” J. Struct. Eng., vol. 132, no. 4, pp. 515–528, Apr. 2006, doi: 10.1061/(ASCE)0733-9445(2006)132:4(515).
 - C. Yu and B. W. Schafer, “Local Buckling Tests on Cold-Formed Steel Beams,” J. Struct. Eng., vol. 129, no. 12, pp. 1596–1606, Dec. 2003, doi: 10.1061/(ASCE)0733-9445(2003)129:12(1596).
+
+### Columns
+
+- C. Ding, “Strength and Behavior of Advanced High Strength Steel Structural Components,” PhD Dissertation, Johns Hopkins University, Baltimore, MD, 2022. [Online]. Available: https://jscholarship.library.jhu.edu/handle/1774.2/67848.
+
+## Roadmap
 
 ## Licence
 
---
+++ References/Beams/Cheng-2006-Distortional Buckling Tests on Cold-Formed Steel Beams/PlotRawData.jl
@@ -0,0 +1,38 @@
+using DelimitedFiles
+using CairoMakie, MathTeXEngine
+CairoMakie.activate!(type=:svg)
+
+# Extract test labels:
+Directory = "References/Beams/Cheng-2006-Distortional Buckling Tests on Cold-Formed Steel Beams/"
+rm(string(Directory, "RawData/.DS_Store"), force=true)
+Filenames = readdir(string(Directory, "RawData/"))
+NumTests = length(Filenames)
+
+# Extract load-deformation curves:
+LDCurves = Vector{Matrix{Float64}}(undef, NumTests)
+for i in 1:NumTests
+    LDCurves[i] = readdlm(string(Directory, "RawData/", Filenames[i]), ',')
+end
+
+# Plot:
+begin
+    F = Figure(resolution=72 .* (12, 9), fonts=(; regular=texfont()), fontsize=14)
+
+    Label(F[0, 1][1, 1], L"Distortional Buckling Tests on Cold-Formed Steel Beams$$", fontsize=16, tellwidth=false)
+    Label(F[0, 1][2, 1], L"Cheng Yu and Benjamin W. Schafer (2006)$$", fontsize=12, tellwidth=false)
+
+    A = Axis(F[1, 1],
+        xlabel=L"Actuator displacement, $\Delta$ (mm)", ylabel=L"Actuator force, $F$ (kN)",
+        xminorticks=IntervalsBetween(5), yminorticks=IntervalsBetween(5),
+        xminorticksvisible=true, yminorticksvisible=true,
+        xminorgridvisible=true, yminorgridvisible=true,
+        limits=(0, nothing, 0, nothing), aspect=4 / 3)
+
+    for i in 1:NumTests
+        lines!(LDCurves[i])
+    end
+
+    F
+end
+
+save(string(Directory, "RawData.pdf"), F)
